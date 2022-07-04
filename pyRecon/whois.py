b48cee3f4cbe06c76480a265fde53aa755ff5125
KaranvirSinghJ <ksjaura15@gmail.com>
2022-07-04 00:55:14 -0700

    website scanner

---
+++ pyRecon/whois.py
@@ -0,0 +1,11 @@
+import os
+from unittest import result
+
+
+def get_whois(url):
+    command = "whois " + url
+    process = os.popen(command)
+    results = str(process.read())
+    return results
+
+print(get_whois('github.com'))
